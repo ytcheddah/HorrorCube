c4915e9e68b2d1a0a78d923174ddf12bc6a71a5f
connormooney500 <91298580+connormooney500@users.noreply.github.com>
2025-01-23 16:30:28 -0700

    MC Movement
    added main character movement attached to the main character scene in the character2d node.

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,5 +1,32 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const max_speed = 300
+const accel = 1500
+const friction = 900
+
+var input = Vector2.ZERO
+
+
+func _physics_process(delta):
+	player_movement(delta)
+	
+func get_input():
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return input.normalized()
+	
+func player_movement(delta):
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * (friction * delta)
+		else:
+			velocity = Vector2.ZERO
+	else:
+		velocity += (input * accel * delta)
+		velocity = velocity.limit_length(max_speed)
+		
+	move_and_slide()
+		
