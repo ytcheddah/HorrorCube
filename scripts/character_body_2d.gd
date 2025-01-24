f0fb3a0661f732066aa309a6e05b6da2fe149423
connormooney500 <91298580+connormooney500@users.noreply.github.com>
2025-01-23 17:37:23 -0700

    added MC animation
    added animation since everything was already set up, the code was actually pretty simple and made sense

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,24 +1,29 @@
 extends CharacterBody2D
 
-
 const max_speed = 300
 const accel = 1500
 const friction = 900
 
 var input = Vector2.ZERO
+var animation_player : AnimationPlayer
+var last_direction : String = ""  # Tracks the last direction the character was walking
 
+func _ready():
+	# Make sure we have an AnimationPlayer node in the scene
+	animation_player = $AnimationPlayer  # Assuming your AnimationPlayer is a direct child of this node
 
 func _physics_process(delta):
 	player_movement(delta)
-	
+	update_animation()
+
 func get_input():
 	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	return input.normalized()
-	
+
 func player_movement(delta):
 	input = get_input()
-	
+
 	if input == Vector2.ZERO:
 		if velocity.length() > (friction * delta):
 			velocity -= velocity.normalized() * (friction * delta)
@@ -27,6 +32,30 @@ func player_movement(delta):
 	else:
 		velocity += (input * accel * delta)
 		velocity = velocity.limit_length(max_speed)
-		
+
 	move_and_slide()
-		
+
+func update_animation():
+	if input == Vector2.ZERO:
+		if last_direction != "":
+			# When the character stops, play the first frame of the last walking animation
+			animation_player.play(last_direction + "_idle")
+		else:
+			# If there's no last direction, do nothing or play a default idle (optional)
+			pass
+	else:
+		# Determine movement direction and play the corresponding animation
+		if abs(input.x) > abs(input.y):  # Moving left or right
+			if input.x > 0:
+				animation_player.play("walkLeft")
+				last_direction = "walkLeft"
+			else:
+				animation_player.play("walkRight")
+				last_direction = "walkRight"
+		else:  # Moving up or down
+			if input.y > 0:
+				animation_player.play("walkDown")
+				last_direction = "walkDown"
+			else:
+				animation_player.play("walkUp")
+				last_direction = "walkUp"
